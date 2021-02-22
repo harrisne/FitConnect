042bd1b7e80435d07de22d4ec5a1844f1db52c43
nharris41 <nharris41@gatech.edu>
2021-02-22 13:57:07 -0500

    Edit Account Information Pages

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -6,11 +6,13 @@ import TraineeRegisterScreen from "../screens/TraineeRegisterScreen";
 import TrainerRegisterScreen from "../screens/TrainerRegisterScreen";
 import WelcomeScreen from "../screens/WelcomeScreen"
 import WhoAreYouScreen from "../screens/WhoAreYouScreen"
+import MyAccountTrainer from "../screens/MyAccountTrainerScreen"
 
 const Stack = createStackNavigator();
 
 const AccountNavigator = () => (
   <Stack.Navigator>
+    <Stack.Screen name="MyAccountTrainer" component={MyAccountTrainer} />
     <Stack.Screen name="Welcome" component={WelcomeScreen} />
     <Stack.Screen name="Login" component={LoginScreen} />
     <Stack.Screen name="Home" component={HomeScreen} />
