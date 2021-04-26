10f405c8c9044c5d223844f7dc77a115ad7f7373
nharris41 <nharris41@gatech.edu>
2021-04-26 08:35:17 -0400

    Update Testing.js

--- src/screens/settingsScreens/databaseTesting/Testing.js
+++ src/screens/settingsScreens/databaseTesting/Testing.js
@@ -1,14 +1,39 @@
-// import React, { useState } from 'react'
-// import DatePicker from 'react-native-date-picker'
+import * as React from 'react'
+import { Button } from 'react-native-paper'
+import { TimePickerModal } from 'react-native-paper-dates'
 
-// export default () => {
+export default function TimePickerPage() {
+  const [visible, setVisible] = React.useState(false)
+  const onDismiss = React.useCallback(() => {
+    setVisible(false)
+  }, [setVisible])
 
-//   const [date, setDate] = useState(new Date())
+  const onConfirm = React.useCallback(
+    ({ hours, minutes }) => {
+      setVisible(false);
+      console.log({ hours, minutes });
+    },
+    [setVisible]
+  );
 
-//   return (
-//     <DatePicker
-//       date={date}
-//       onDateChange={setDate}
-//     />
-//   )
-// }
+
+  return (
+    <>
+      <TimePickerModal
+        visible={visible}
+        onDismiss={onDismiss}
+        onConfirm={onConfirm}
+        hours={12} // default: current hours
+        minutes={14} // default: current minutes
+        label="Select time" // optional, default 'Select time'
+        cancelLabel="Cancel" // optional, default: 'Cancel'
+        confirmLabel="Ok" // optional, default: 'Ok'
+        animationType="fade" // optional, default is 'none'
+        locale={'en'} // optional, default is automically detected by your system
+      />
+      <Button onPress={()=> setVisible(true)}>
+        Pick time
+      </Button>
+    </>
+  )
+}
